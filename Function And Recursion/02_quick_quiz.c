61658a60fbc5a8c2ec6cf722481cb184c2bf113e
Arghadatta <arghadatta6660@gmail.com>
2021-09-16 17:25:03 +0600

    Added quick quiz file

---
+++ Function And Recursion/02_quick_quiz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void GoodMorning();
+void GoodAfternoon();
+void GoodNight();
+
+int main()
+{
+    printf("Initializing GoodMorning,GoodAfternoon And GoodNight functions--\n");
+    GoodMorning();
+    GoodAfternoon();
+    GoodNight();
+
+    return 0;
+}
+
+void GoodMorning(){
+    printf("Good Morning\n");
+}
+void GoodAfternoon(){
+    printf("good Afternoon\n");
+}
+void GoodNight(){
+    printf("Good Night\n");
+}
