7a58128073fff01c5904ee6e9e735621832b852c
Arghadatta <arghadatta6660@gmail.com>
2021-09-20 16:53:31 +0600

    Started Pointers

---
+++ Pointer/01_intro.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 100;
+    //printf("%u\n", &a);
+    int *j = &a;
+    printf("The value of a is %d\n", *j);
+    printf("The address of j is %d\n", *(&j));
+    return 0;
+}
