1ac51b4c831cce5e587cd6a2332ffe76a825ba05
Argha_Datta <arghadatta6660@gmail.com>
2021-10-27 12:35:20 +0600

    Edit 04

--- Array/Problems/pr_04.c
+++ Array/Problems/pr_04.c
@@ -24,33 +24,3 @@ int main()
 
     return 0;
 }
-
-// #include<stdio.h>
-
-// int main()
-// {
-//     //let's assume the maximum array size as 100.
-//     int arr[100],size,i;
-
-//     //Get size input from user
-//     printf("Enter array size\n");
-//     scanf("%d",&size);
-
-//     //Get all elements using for loop and store it in array
-//     printf("Enter array elements\n");
-//     for(i = 0; i < size; i++)
-//           scanf("%d",&arr[i]);
-
-//     //print only the positive elements in the array
-//     printf("Elements are\n");
-//     for(i = 0; i < size; i++)
-//     {
-//           //positive numbers are greater than 0.
-//           /*if you want to print the negative numbers,
-//             just change the if condition as arr[i] < 0 */
-//           if(arr[i] > 0)
-//               printf("%d\n",arr[i]);
-//     }
-
-//     return 0;
-// }
