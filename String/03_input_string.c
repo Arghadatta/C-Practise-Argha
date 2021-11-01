03a625db40e3d372cdd3c730d6e8588f595d5cd4
Argha_Datta <arghadatta6660@gmail.com>
2021-11-01 17:17:00 +0600

    String p-02

---
+++ String/03_input_string.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    char str_2[100];
+    printf("Enter your name: ");
+    scanf("%s",str_2);
+    printf("Your name is %s",str_2);
+    return 0;
+}
---
+++ String/04_strcpy_string.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char st[] = "Argha Datta";
+    char st2[100];
+    strcpy(st2 , st);
+    printf("%s",st2);
+    return 0;
+}
